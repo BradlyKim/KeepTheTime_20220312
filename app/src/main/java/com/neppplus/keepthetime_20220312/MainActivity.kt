821f82e646f54440ce54713ad6f43acc7312da7d
BradlyKim <kjh5864@naver.com>
2022-03-12 11:28:00 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/keepthetime_20220312/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.neppplus.keepthetime_20220312
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {}
+
+    fun setValues() {}
+
+}
