1a3739633931b792528d0b8a472a691c1de03fea
BradlyKim <kjh5864@naver.com>
2022-03-21 15:29:40 +0900

    회원가입화면을, BaseActivity 상속으로 변경

--- app/src/main/java/com/neppplus/keepthetime_20220312/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/SignUpActivity.kt
@@ -5,15 +5,24 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_20220312.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
     }
 
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
 
 
 }
