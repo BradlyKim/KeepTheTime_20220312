99bc54580eb4032480db24736b8bf7bd89cbf5d4
BradlyKim <kjh5864@naver.com>
2022-03-22 17:42:39 +0900

    토큰 활용 연습문제 세팅

--- app/src/main/java/com/neppplus/keepthetime_20220312/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/MainActivity.kt
@@ -22,5 +22,11 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        연습용 (임시 기능)
+//        로그인한 사람의 정보를 받아오자 (서버에서)
+//        닉네임을 추출해서 > 텍스트뷰에 반영
+
+
+
     }
 }
