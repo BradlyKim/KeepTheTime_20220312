641ad393c2c2cc4e76c9e98cea40803d41e35c92
BradlyKim <kjh5864@naver.com>
2022-03-22 13:48:48 +0900

    서버가 주는 data: { } 는 별도의 클래스를 만들어서 그 모양을 표현

--- app/src/main/java/com/neppplus/keepthetime_20220312/datas/BasicResponse.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/datas/BasicResponse.kt
@@ -3,6 +3,7 @@ package com.neppplus.keepthetime_20220312.datas
 class BasicResponse(
     val code: Int,
     val message: String,
+    val data: DataResponse,
 
 ) {
 }
---
+++ app/src/main/java/com/neppplus/keepthetime_20220312/datas/DataResponse.kt
@@ -0,0 +1,7 @@
+package com.neppplus.keepthetime_20220312.datas
+
+class DataResponse(
+//    val user: ?? 현재는 넘어감.
+    val token: String,
+) {
+}
