8d9ecf9f469c0a43d04f6b328b55c81abf3156af
BradlyKim <kjh5864@naver.com>
2022-03-22 23:41:29 +0900

    내 정보 프래그먼트 - BaseFragment 상속받아 완성

---
+++ app/src/main/java/com/neppplus/keepthetime_20220312/fragments/MyProfileFragment.kt
@@ -0,0 +1,34 @@
+package com.neppplus.keepthetime_20220312.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_20220312.R
+
+class MyProfileFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setupValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setupValues() {
+
+    }
+
+}
