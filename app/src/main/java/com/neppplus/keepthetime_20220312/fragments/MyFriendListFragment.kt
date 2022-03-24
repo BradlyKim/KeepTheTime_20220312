9b0473096911aab8c05a9b4b0809d198f8932c2e
BradlyKim <kjh5864@naver.com>
2022-03-24 19:39:51 +0900

    내 친구 목록을 서버에서 받아오기

--- app/src/main/java/com/neppplus/keepthetime_20220312/fragments/MyFriendListFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/fragments/MyFriendListFragment.kt
@@ -6,8 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.keepthetime_20220312.R
+import com.neppplus.keepthetime_20220312.datas.BasicResponse
+import com.neppplus.keepthetime_20220312.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyFriendListFragment : BaseFragment() {
+
+    val mMyFriendList = ArrayList<UserData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +35,30 @@ class MyFriendListFragment : BaseFragment() {
     }
 
     override fun setupValues() {
+
+        getMyFriendListFromServer()
+
+    }
+
+    fun getMyFriendListFromServer(){
+
+        apiList.getRequestFriendList(
+            "my"
+        ).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+                    val br = response.body()!!
+                    mMyFriendList.addAll(br.data.friends)
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
     }
 
