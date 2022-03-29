ff1cd06e6b0e50f20b250aa08f4753bb1fd490f2
BradlyKim <kjh5864@naver.com>
2022-03-29 17:31:55 +0900

    어떤 메뉴 아이템을 골랐냐에 따라 뷰페이저 페이지 이동 처리

--- app/src/main/java/com/neppplus/keepthetime_20220312/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.neppplus.keepthetime_20220312
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_20220312.adapters.MainViewPager2Adapter
-import com.neppplus.keepthetime_20220312.adapters.MainViewPagerAdapter
 import com.neppplus.keepthetime_20220312.databinding.ActivityMainBinding
-import com.neppplus.keepthetime_20220312.datas.BasicResponse
-import com.neppplus.keepthetime_20220312.utils.ContextUtil
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -26,6 +19,22 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        바텀 네비게이션의 이벤트 처리
+        binding.mainBottomNav.setOnItemSelectedListener {
+
+//            it 변수 : 선택된 메뉴가 뭔지? 알려줌
+//            it 변수의 id값에 따라, 페이지 이동
+
+            if (it.itemId == R.id.home) {
+                binding.mainViewPager2.currentItem = 0
+            }
+            else {
+                binding.mainViewPager2.currentItem = 1
+            }
+
+            return@setOnItemSelectedListener true
+        }
 
     }
 
