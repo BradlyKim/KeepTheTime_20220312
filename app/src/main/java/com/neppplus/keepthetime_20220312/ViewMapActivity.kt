08d3a8595c74b22a611a34cdf3f1e803030b5532
BradlyKim <kjh5864@naver.com>
2022-04-06 12:29:35 +0900

    지도 아이콘 눌리면, 어떤 약속을 보여줄건지 첨부

--- app/src/main/java/com/neppplus/keepthetime_20220312/ViewMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/ViewMapActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_20220312.databinding.ActivityViewMapBinding
+import com.neppplus.keepthetime_20220312.datas.AppointmentData
 
 class ViewMapActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewMapBinding
 
+    lateinit var mAppointmentData: AppointmentData    // 화면에 넘겨준 약속 자체
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_map)
+
+        mAppointmentData = intent.getSerializableExtra("appointment") as AppointmentData
+
         setupEvents()
         setValues()
     }
@@ -21,6 +27,9 @@ class ViewMapActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        약속이름을 화면의 제목으로 설정
+        txtTitle.text = mAppointmentData.title
 
     }
 }
