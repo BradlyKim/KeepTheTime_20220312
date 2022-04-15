e0b075034552b5dce8435139ea96c1a6cc3a9f01
BradlyKim <kjh5864@naver.com>
2022-04-15 12:58:57 +0900

    백그라운드에서 핸들러를 이용해 UI쓰레드에 토스트 띄우기 요청

--- app/src/main/java/com/neppplus/keepthetime_20220312/fcm/MyFCM.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220312/fcm/MyFCM.kt
@@ -1,9 +1,13 @@
 package com.neppplus.keepthetime_20220312.fcm
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+
 
 class MyFCM : FirebaseMessagingService() {
 
@@ -13,6 +17,16 @@ class MyFCM : FirebaseMessagingService() {
 //        화면에 앱이 켜져있는 상태로 알림이 오면 실행할 코드
         val title = message.notification!!.title
         Log.d("푸시알림수신", title!!)
+
+//        토스트로 제목 띄워보기
+//        서비스의 일종 => 백그라운드 처리 => 백그라운드가 UI를 건드리면, 위험동작으로 간주되어 앱 강제 종료됨.
+//        UI 쓰레드에게 토스트 띄우기를 부탁해보자.
+        val myHandler = Handler( Looper.getMainLooper() )   // UI 쓰레드와 연결되는 핸들러 생성
+        myHandler.post {
+            Toast.makeText(this, title!!, Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 
 }
